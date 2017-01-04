63ad0a418cdb24ce322f9fe950d6c2c5fa566985
weezing <weezing@protonmail.com>
2017-01-05 00:47:18 +0100

    Add show action and view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: :show
+
   def index
     @books = BookQuery.new(books_params).results
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
