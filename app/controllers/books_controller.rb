7e48297c02d7e95f51e7716435e106eaa89a5e8a
weezing <weezing@protonmail.com>
2017-01-05 02:16:20 +0100

    Add create action in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :show]
+  before_action :authenticate_user!, only: [:new, :create, :show]
 
   def index
     @books = BookQuery.new(books_params).results
@@ -7,6 +7,17 @@ class BooksController < ApplicationController
 
   def new
     @form = CreateBookForm.new
+  end
+
+  def create
+    @form = CreateBookForm.new(book_form_params)
+    service = CreateBookService.new(@form)
+
+    if service.call
+      redirect_to books_path, notice: _("Book added successfully")
+    else
+      render :new
+    end
   end
 
   def show
@@ -17,5 +28,16 @@ class BooksController < ApplicationController
 
   def books_params
     params.permit(:per, :page)
+  end
+
+  def book_form_params
+    params.require(:book_form).permit(
+      :title,
+      :author,
+      :year,
+      :pages,
+      :isbn,
+      :quantity
+    )
   end
 end
