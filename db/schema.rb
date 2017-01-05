53a94944b11436fcf55c089b7d229746a7afff6b
weezing <weezing@protonmail.com>
2017-01-05 01:31:13 +0100

    Add isbn to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104224631) do
+ActiveRecord::Schema.define(version: 20170105002526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20170104224631) do
     t.integer  "current_quantity"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "isbn"
   end
+
+  add_index "books", ["isbn"], name: "index_books_on_isbn", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@
     pages: rand(50..1000),
     year: rand(1450..Time.zone.now.year),
     quantity: 5,
-    current_quantity: 5
+    current_quantity: 5,
+    isbn: Faker::Code.isbn
   )
+end
+
+
+Book.all.each do |book|
+  book.isbn = Faker::Code.isbn
+  book.save
 end
