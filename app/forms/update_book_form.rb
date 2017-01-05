42cbe5d58e64884b222fcb419f9599bf0189b85b
weezing <weezing@protonmail.com>
2017-01-05 13:57:45 +0100

    Add validations to update book form

--- app/forms/update_book_form.rb
+++ app/forms/update_book_form.rb
@@ -11,6 +11,23 @@ class UpdateBookForm
     :isbn
   )
 
+  validates :title, :author, :pages, :year, :current_quantity, :isbn, presence: true
+  validates_numericality_of :pages, :year, :current_quantity
+  validates :pages, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1
+  }
+  validates :year, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1450,
+    less_than_or_equal_to: Time.zone.now.year
+  }
+  validates :current_quantity, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validates_with IsbnUniquenessValidator
+
   def initialize(book_attributes, form_attributes = {})
     super book_attributes.merge(form_attributes)
   end
@@ -21,7 +38,7 @@ class UpdateBookForm
       author: author,
       pages: pages,
       year: year,
-      quantity: quantity,
+      quantity: current_quantity,
       current_quantity: current_quantity,
       isbn: isbn
     }
